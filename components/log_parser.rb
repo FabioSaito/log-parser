ad615dcfc89ffc11efff702eadc0e3e85734ba01
Fabio Saito <fabio.h.saito@gmail.com>
2022-01-20 15:56:51 -0300

    refactor: add load_file to initialize and nested describe in log_parser_spec

--- components/log_parser.rb
+++ components/log_parser.rb
@@ -3,20 +3,16 @@ class LogParser
     if File.exists?(file_path)
       @file_path = file_path
       @file_str = ""
+      file = File.open(@file_path)
+      @file_str = file.readlines.map(&:chomp)
+      file.close
     else
       raise "Arquivo não encontrado em: #{file_path}"
     end
-  end
-
-  def load_data
-    file = File.open(@file_path)
-    @file_str = file.readlines.map(&:chomp)
-    file.close
   end
 
   def get_first_line
     load_data if @file_str == ""
     @file_str[0]
   end
-
 end
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,17 +1,18 @@
 require_relative '../components/log_parser'
-
-describe '#get_first_line' do
-  it 'check first line' do
-    file_path = "./spec/fixtures/files/games_test.log"
-    first_expected_line = "  0:00 ------------------------------------------------------------"
-    expect(LogParser.new(file_path).get_first_line).to eq(first_expected_line)
+describe LogParser do
+  describe '#get_first_line' do
+    it 'check first line' do
+      file_path = "./spec/fixtures/files/games_test.log"
+      first_expected_line = "  0:00 ------------------------------------------------------------"
+      expect(LogParser.new(file_path).get_first_line).to eq(first_expected_line)
+    end  
   end  
-end  
 
-describe '#initialize' do
-  it 'raise error when invalid file' do
-    file_path = "./spec/fixtures/files/games2.log"
-    err_message = "Arquivo não encontrado em: #{file_path}"
-    expect{LogParser.new(file_path)}.to raise_error(err_message)
-  end
-end  
+  describe '#initialize' do
+    it 'raise error when invalid file' do
+      file_path = "./spec/fixtures/files/games2.log"
+      err_message = "Arquivo não encontrado em: #{file_path}"
+      expect{LogParser.new(file_path)}.to raise_error(err_message)
+    end
+  end  
+end

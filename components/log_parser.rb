8d1b766fb8fee4be05e797f703763d8f0b248432
Fabio Saito <fabio.h.saito@gmail.com>
2022-01-20 17:16:19 -0300

    feat: count lines on initialize

--- components/log_parser.rb
+++ components/log_parser.rb
@@ -1,11 +1,14 @@
+require "json"
 class LogParser
   def initialize(file_path)
     if File.exists?(file_path)
       @file_path = file_path
       @file_str = ""
+      @file_lines = -1
       file = File.open(@file_path)
       @file_str = file.readlines.map(&:chomp)
       file.close
+      count_file_lines
     else
       raise "Arquivo não encontrado em: #{file_path}"
     end
@@ -15,4 +18,18 @@ class LogParser
     load_data if @file_str == ""
     @file_str[0]
   end
+
+  def count_file_lines
+    count_lines = 0
+    @file_str.each do |line|
+      count_lines += 1
+    end
+    @file_lines = count_lines
+  end
+
+  def print_file_json
+    file_to_json = {:lines => @file_lines}.to_json
+    puts "#{@file_path}: #{JSON.pretty_generate(JSON.parse(file_to_json))}"
+  end
+
 end
--- main.rb
+++ main.rb
@@ -4,4 +4,12 @@ require_relative "./components/handling_functions"
 file_path = "src/games.log"
 
 file_obj = LogParser.new(file_path)
+
+# --- Tarefa 1 ---
+puts "\n[Tarefa 1 - Imprimir a primeira linha]"
 puts file_obj.get_first_line
+
+# --- Tarefa 2 ---
+
+puts "\n[Tarefa 2 - Imprimir json conforme modelo]"
+file_obj.print_file_json

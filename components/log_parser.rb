8f526fcb585e0b42e205f0282918568130a62359
Fabio Saito <fabio.h.saito@gmail.com>
2022-01-21 11:14:55 -0300

    refactor: make count_file_lines a private method

--- components/log_parser.rb
+++ components/log_parser.rb
@@ -16,12 +16,14 @@ class LogParser
   def get_first_line
     @file_str[0]
   end
-  
-  def count_file_lines
-    @file_str.count
-  end
 
   def output
     JSON.pretty_generate(@file_path => { lines: count_file_lines })
+  end
+
+  private
+  
+  def count_file_lines
+    @file_str.count
   end
 end

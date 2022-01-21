dc42390aa15b068aa4bb112e034e1db1861af227
Fabio Saito <fabio.h.saito@gmail.com>
2022-01-21 13:28:58 -0300

    feat: implement get_players method to list all players in log

--- components/log_parser.rb
+++ components/log_parser.rb
@@ -1,4 +1,5 @@
 require "json"
+require 'set'
 
 class LogParser
   def initialize(file_path)
@@ -17,8 +18,25 @@ class LogParser
     @file_str[0]
   end
 
+  def get_players
+    @players = Set[]
+    temp_array = []
+    @file_str.each do |line|
+      temp_array =  line.split(" ")
+      case temp_array[1]
+      when "ClientUserinfoChanged:"
+        @players.add(line.match(/n\\(.*?)\\t/).captures[0])
+
+      when "Kill:"
+        @players.add(line.match(/\d+ \d+ \d+\: (.*?) killed/).captures[0])
+        @players.add(line.match(/killed (.*?) by/).captures[0]) 
+      end
+    end
+    @players.to_a
+  end
+
   def output
-    JSON.pretty_generate(@file_path => { lines: count_file_lines })
+    JSON.pretty_generate(@file_path => { lines: count_file_lines, players: get_players })
   end
 
   private
